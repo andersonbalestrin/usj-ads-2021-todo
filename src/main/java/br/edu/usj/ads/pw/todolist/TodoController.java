8ec0962d1c1fb7b5a4d22f2430d6c3f8128dd94e
andersonbalestrin <anderson.balestrin@gmail.com>
2021-10-29 20:51:31 -0300

    primeiro commit

---
+++ src/main/java/br/edu/usj/ads/pw/todolist/TodoController.java
@@ -0,0 +1,36 @@
+package br.edu.usj.ads.pw.todolist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class TodoController {
+List<String> list = new ArrayList<>();
+
+    @GetMapping (value = "/")
+    public ModelAndView getIndex() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todo_list", list);
+        return modelAndView;
+    }
+
+    @PostMapping(value="/add")
+    public ModelAndView postAdd(@RequestParam String todo) {
+       list.add(todo);     
+
+       System.out.println(todo);
+
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todo_list", list);
+        return modelAndView;
+    }
+    
+    
+}
